767c17120a36df353aba64932cb7889fd5cc3fce
Kurt Galiatsatos <soad1789@gmail.com>
2016-04-30 17:52:16 +0000

    Added profiles#new action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,11 @@
 class ProfilesController < ApplicationController
-  
+  def new
+    # Have a form show up, allowing the user to fill out their *OWN* profile info
+    
+    # Create instance variable tied to unique user ID
+    @user = User.find( params[:user_id] )
+    
+    # This build_profile() method becomes available to us because we established the model associations between the users and profiles
+    @profile = @user.build_profile
+  end
 end
