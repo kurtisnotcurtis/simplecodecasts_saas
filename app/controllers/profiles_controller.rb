63106e921860be255182ab529b5a29a3d32a9af2
Kurt Galiatsatos <soad1789@gmail.com>
2016-05-03 19:54:42 +0000

    Added profiles form and create action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -8,4 +8,8 @@ class ProfilesController < ApplicationController
     # This build_profile() method becomes available to us because we established the model associations between the users and profiles
     @profile = @user.build_profile
   end
+  
+  def create
+    @user = User.find( params[:user_id] )
+  end
 end
