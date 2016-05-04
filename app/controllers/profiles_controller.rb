50e3115b99b7bf7f8eff002f68f4af4b21b14f56
Kurt Galiatsatos <soad1789@gmail.com>
2016-05-04 04:44:26 +0000

    Added before filters to check correct user before executing user and profile controller actions

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,8 @@
 class ProfilesController < ApplicationController
+  # You must be a user to perform any actions with profiles
+  before_action :authenticate_user!
+  # The current user can only edit their OWN profile, not anybody else's.
+  before_action :only_current_user
   def new
     # Have a form show up, allowing the user to fill out their *OWN* profile info
 
@@ -25,10 +29,26 @@ class ProfilesController < ApplicationController
     @profile = @user.profile
   end
 
+  def update
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile Updated!"
+      redirect_to user_path( params[:user_id] )
+    else
+      render action: :edit
+    end
+  end
+
   private
   def profile_params
     # Require and only permit the following parameters sent from the profile form
     params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
+  
+  def only_current_user
+    @user = User.find( params[:user_id] )
+    redirect_to(root_url) unless @user == current_user
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # You must be a user to perform any actions with profiles
+  before_action :authenticate_user!
+
   def show
     @user = User.find( params[:id] )
   end
