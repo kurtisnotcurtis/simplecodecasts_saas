41c901bb7abb2329a64a0fa9949bfda63084b187
Kurt Galiatsatos <soad1789@gmail.com>
2016-05-07 00:32:05 +0000

    Added index action and updated link to index on homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,11 @@ class UsersController < ApplicationController
   # You must be a user to perform any actions with profiles
   before_action :authenticate_user!
 
+  def index
+  end
+
   def show
     @user = User.find( params[:id] )
   end
+
 end
